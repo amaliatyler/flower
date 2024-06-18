4b27714471bb24133c96da3fc6d0d9aae71caf93
AATayler <amaliatyler@gmail.com>
2024-06-18 17:35:14 +0300

    Добавлен компонент логотипа

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
+import Logo from '../Logo/Logo';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
-          <a href="/" className="header__logo">
-            <span className="header__logo_back">L</span>
-            <span className="header__logo_front">F</span>
-          </a>
+          <Logo />
           <Nav />
         </div>
       </div>
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <a href="/" class="logo">
+      <span class="logo-back">L</span>
+      <span class="logo-front">F</span>
+    </a>
+  );
+}
